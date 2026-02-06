dd7932f4ac6fca614a1f7be3c2a44e4a83fda2e6
lf <contact@loganforman.com>
2026-02-06 07:39:00 -0700

    make links, images, explains work

--- md.c
+++ md.c
@@ -123,7 +123,7 @@ bool match_prefix(str *s, char *pre, s32 *tok_len) {
   }
   *s = match? str_skip(*s, i) : *s;
   *tok_len = match? i : 0; 
-  return match;
+  return match && !pre[i];
 }
 
 // returns remaining string to be parsed
@@ -131,6 +131,14 @@ str parse_inline(Arena *a, Text *p) {
   str s = p->s;
   s32 tok_len = 0;
   enum TextStyle stop = p->type;
+
+  if (stop == LINK || stop == IMAGE || stop == EXPLAIN) {
+    s64 loc = str_find_char(s, ')');
+    ASSERT(loc >= 0, "parenthesis must close!");
+    p->s = str_first(s, loc);
+    return str_skip(s, loc+1);
+  }
+
   while (s.len > 0) {
     while (s.len > 0 && (char_is_whitespace(s.str[0]) || char_is_alphanum(s.str[0]))) {
       s = str_skip(s, 1);
@@ -142,11 +150,10 @@ str parse_inline(Arena *a, Text *p) {
     else if (match_prefix(&s, "*", &tok_len))  { tok = ITALIC; }
     else if (match_prefix(&s, "~~", &tok_len)) { tok = STRUCK; }
     else if (match_prefix(&s, "`", &tok_len))  { tok = CODE_INLINE; }
-    // TODO
-    // else if (match_prefix(&s, "@(", &tok_len)) { tok = LINK; }
-    // else if (match_prefix(&s, "!(", &tok_len)) { tok = IMAGE; }
-    // else if (match_prefix(&s, "?(", &tok_len)) { tok = EXPLAIN; }
     else if (match_prefix(&s, "|", &tok_len))  { tok = TABLE_CELL; }
+    else if (match_prefix(&s, "@(", &tok_len)) { tok = LINK; }
+    else if (match_prefix(&s, "!(", &tok_len)) { tok = IMAGE; }
+    else if (match_prefix(&s, "?(", &tok_len)) { tok = EXPLAIN; }
     else { s = str_skip(s, 1); }
 
     if (tok != NONE) {
@@ -223,7 +230,7 @@ void append(Buf *b, u8 *data, s32 len) {
   b->err |= amount < len;
 }
 #define append_strl(b, sl) append(b, (u8*)sl, sizeof(sl"")-1)
-#define append_str(b, s) append(b, (s).str, (s).len);
+void append_str(Buf *b, str s) { append(b, s.str, s.len); }
 
 void render_inline(Buf *out, Text *t) {
   const str tags[TEXT_STYLES] = {
@@ -235,17 +242,42 @@ void render_inline(Buf *out, Text *t) {
   // LINK, IMAGE, EXPLAIN, // not covered TODO handle special cases
   };
   for (; t; t = t->next) {
-    str tag = tags[t->type]; 
-    if (tag.len > 0) {
-      append_str(out, tag);
-    }
+    str s = t->s;
+    if (t->type == LINK) {
+      append_strl(out, "<a href='");
+      append_str(out, str_cut_char(&s, ' '));
+      append_strl(out, "'>");
+      append_str(out, s);
+      append_strl(out, "</a>");
+    } else if (t->type == EXPLAIN) {
+      append_strl(out, "<abbr title='");
+      append_str(out, str_cut_char(&s, ','));
+      append_strl(out, "'>");
+      append_str(out, s);
+      append_strl(out, "</abbr>");
+    } else if (t->type == IMAGE) {
+      if (str_has_suffix(s, strl(".mp4"))) {
+        append_strl(out, "<video controls><source src='");
+        append_str(out, s);
+        append_strl(out, "' type='video/mp4'></video>");
+      } else {
+        append_strl(out, "<img src='");
+        append_str(out, s);
+        append_strl(out, "'>");
+      }
+    } else {
+      str tag = tags[t->type]; 
+      if (tag.len > 0) {
+        append_str(out, tag);
+      }
 
-    append_str(out, t->s);
-    render_inline(out, t->child);
+      append_str(out, s);
+      render_inline(out, t->child);
 
-    if (tag.len > 0) {
-      append_strl(out, "</");
-      append_str(out, str_skip(tag, 1));
+      if (tag.len > 0) {
+        append_strl(out, "</");
+        append_str(out, str_skip(tag, 1));
+      }
     }
   }
 }
@@ -278,114 +310,112 @@ void render_wrap(Buf *out, Block *b, Wrap w) {
   }
 }
 
-str render_html(Buf *out, Block *first) {
-  for (Block *b = first; b; b = b->next) {
-    render_wrap(out, b, WRAP(PARAGRAPH, "<p>\n", "</p>\n", "", ""));
-    render_wrap(out, b, WRAP(QUOTE, "<blockquote><p>\n", "</p></blockquote>\n", "", ""));
-    render_wrap(out, b, WRAP(ORD_LIST, "<ol>\n", "</ol>\n", "<li>", "</li>\n"));
-    render_wrap(out, b, WRAP(UN_LIST, "<ul>\n", "</ul>\n", "<li>", "</li>\n"));
-    render_wrap(out, b, WRAP(RULE, "<hr>\n", "", "", ""));
+str append_html(Buf *out, Block *b) {
+  render_wrap(out, b, WRAP(PARAGRAPH, "<p>\n", "</p>\n", "", ""));
+  render_wrap(out, b, WRAP(QUOTE, "<blockquote><p>\n", "</p></blockquote>\n", "", ""));
+  render_wrap(out, b, WRAP(ORD_LIST, "<ol>\n", "</ol>\n", "<li>", "</li>\n"));
+  render_wrap(out, b, WRAP(UN_LIST, "<ul>\n", "</ul>\n", "<li>", "</li>\n"));
+  render_wrap(out, b, WRAP(RULE, "<hr>\n", "", "", ""));
 
-    if (b->type == TABLE) {
-      append_strl(out, "<table class=");
-      append_str(out, b->id);
-      append_strl(out, ">\n");
-      render_wrap(out, b, WRAP(TABLE, "", "</table>\n", "<tr>", "</tr>\n"));
+  if (b->type == TABLE) {
+    append_strl(out, "<table class=");
+    append_str(out, b->id);
+    append_strl(out, ">\n");
+    render_wrap(out, b, WRAP(TABLE, "", "</table>\n", "<tr>", "</tr>\n"));
+  }
+
+  if (b->type == HEADING) {
+    u8 n = '0' + b->num;
+    append_strl(out, "<h");
+    append(out, &n, 1);
+    append_strl(out, " id='");
+    append_str(out, b->id);
+    append_strl(out, "'>");
+
+    render_inline(out, b->text);
+
+    append_strl(out, "</h");
+    append(out, &n, 1);
+    append_strl(out, ">");
+  }
+
+  if (b->type == CODE) {
+    char id[8];
+    if (b->id.len == 0) {
+      b->id.len = snprintf(id, sizeof(id), "code%03d", b->num);
+      b->id.str = (u8*) id;
     }
+    append_strl(out, "<code id='");
+    append_str(out, b->id);
+    append_strl(out, "'><pre>\n");
+    s32 line = 1;
+    s32 in_comment = 0;
+    #define COMMENT_SPAN "<span style='color: var(--comment);'>"
+    for (Text *t = b->text; t; t = t->next, line++) {
+      char id[32]; 
+      s32 id_len = snprintf(id, sizeof(id), "%.*s-%d", (s32)b->id.len, b->id.str, line);
+      append_strl(out, "<span id='"); 
+      append(out, (u8*) id, id_len);
+      append_strl(out, "'><a href='#"); 
+      append(out, (u8*) id, id_len);
+      append_strl(out, "' aria-hidden='true'></a>"); 
 
-    if (b->type == HEADING) {
-      u8 n = '0' + b->num;
-      append_strl(out, "<h");
-      append(out, &n, 1);
-      append_strl(out, " id='");
-      append_str(out, b->id);
-      append_strl(out, "'>");
-
-      render_inline(out, b->text);
-
-      append_strl(out, "</h");
-      append(out, &n, 1);
-      append_strl(out, ">");
-    }
-
-    if (b->type == CODE) {
-      char id[8];
-      if (b->id.len == 0) {
-        b->id.len = snprintf(id, sizeof(id), "code%03d", b->num);
-        b->id.str = (u8*) id;
+      if (in_comment == 2) {
+        append_strl(out, COMMENT_SPAN);
       }
-      append_strl(out, "<code id='");
-      append_str(out, b->id);
-      append_strl(out, "'><pre>\n");
-      s32 line = 1;
-      s32 in_comment = 0;
-      #define COMMENT_SPAN "<span style='color: var(--comment);'>"
-      for (Text *t = b->text; t; t = t->next, line++) {
-        char id[32]; 
-        s32 id_len = snprintf(id, sizeof(id), "%.*s-%d", (s32)b->id.len, b->id.str, line);
-        append_strl(out, "<span id='"); 
-        append(out, (u8*) id, id_len);
-        append_strl(out, "'><a href='#"); 
-        append(out, (u8*) id, id_len);
-        append_strl(out, "' aria-hidden='true'></a>"); 
 
-        if (in_comment == 2) {
+      char in_string = 0;
+      str s = t->s;
+      s32 i = 0;
+      while (s.len > 0) {
+        while (i < s.len && (char_is_whitespace(s.str[i]) || char_is_alphanum(s.str[i]))) {
+          i++;
+        }
+        append(out, s.str, i);
+        s = str_skip(s, i);
+        i = 0;
+        if (str_has_prefix(s, strl("//"))) {
+          in_comment = 1;
           append_strl(out, COMMENT_SPAN);
-        }
-
-        char in_string = 0;
-        str s = t->s;
-        s32 i = 0;
-        while (s.len > 0) {
-          while (i < s.len && (char_is_whitespace(s.str[i]) || char_is_alphanum(s.str[i]))) {
-            i++;
-          }
-          append(out, s.str, i);
-          s = str_skip(s, i);
-          i = 0;
-          if (str_has_prefix(s, strl("//"))) {
-            in_comment = 1;
-            append_strl(out, COMMENT_SPAN);
-            i = 2;
-          } else if (str_has_prefix(s, strl("/*"))) {
-            in_comment = 2;
-            append_strl(out, COMMENT_SPAN);
-            i = 2;
-          } else if (in_comment == 2 && str_has_prefix(s, strl("*/"))) {
-            in_comment = 0;
-            i = 2;
-          } else if (s.str[0] == '<') {
-            append_strl(out, "&lt;");
-            s = str_skip(s, 1);
-          } else if (s.str[0] == '>') {
-            append_strl(out, "&gt;");
-            s = str_skip(s, 1);
-          } else if (s.str[0] == '&') {
-            append_strl(out, "&amp;");
-            s = str_skip(s, 1);
-          } else if (s.str[0] == '\'' || s.str[0] == '"') {
-            if (in_comment == 0) {
-              if (in_string == 0)  {
-                append_strl(out, "<span style='color: var(--red);'>");
-              } else if (in_string == s.str[0]) {
-                append_strl(out, "</span>");
-              }
+          i = 2;
+        } else if (str_has_prefix(s, strl("/*"))) {
+          in_comment = 2;
+          append_strl(out, COMMENT_SPAN);
+          i = 2;
+        } else if (in_comment == 2 && str_has_prefix(s, strl("*/"))) {
+          in_comment = 0;
+          i = 2;
+        } else if (s.str[0] == '<') {
+          append_strl(out, "&lt;");
+          s = str_skip(s, 1);
+        } else if (s.str[0] == '>') {
+          append_strl(out, "&gt;");
+          s = str_skip(s, 1);
+        } else if (s.str[0] == '&') {
+          append_strl(out, "&amp;");
+          s = str_skip(s, 1);
+        } else if (s.str[0] == '\'' || s.str[0] == '"') {
+          if (in_comment == 0) {
+            if (in_string == 0)  {
+              append_strl(out, "<span style='color: var(--red);'>");
+            } else if (in_string == s.str[0]) {
+              append_strl(out, "</span>");
             }
-            i = 1;
-          } else {
-            i = 1;
           }
+          i = 1;
+        } else {
+          i = 1;
         }
-
-        if (in_comment > 0) {
-          if (in_comment == 1) in_comment = 0;
-          append_strl(out, "</span>");
-        }
-        append_strl(out, "</span>\n");
-
       }
-      append_strl(out, "</pre></code>\n");
+
+      if (in_comment > 0) {
+        if (in_comment == 1) in_comment = 0;
+        append_strl(out, "</span>");
+      }
+      append_strl(out, "</span>\n");
+
     }
+    append_strl(out, "</pre></code>\n");
   }
 
   return (str){ out->buf, out->len };
@@ -409,36 +439,78 @@ str read_file(Arena *a, const char *path) {
   return out;
 }
 
+str header;
+str footer;
+
+Arena a;
+Buf out;
+Buf rss;
+
+void make_page(Block *first) {
+  // append_str(&out, header);
+  for (Block *b = first; b; b = b->next) {
+    if (b->type == SPECIAL) {
+
+    } else {
+      append_html(&out, b);
+    }
+  }
+  // append_str(&out, footer);
+
+}
+
+
 int main(int argc, char *argv[]) {
   UNUSED(argc);
   UNUSED(argv);
-  Arena a = Arena_new((Arena){ .size = MB(32) });
+  a = Arena_new((Arena){ .size = MB(32) });
 
-  str header = read_file(&a, "header.html");
-  str footer = read_file(&a, "footer.html");
-  // str rss_header = read_file(&a, "rss-header.xml");
+  header = read_file(&a, "header.html");
+  footer = read_file(&a, "footer.html");
+  str rss_header = read_file(&a, "rss-header.xml");
 
-  Buf out = {0};
   out.cap = MB(2);
   out.buf = Arena_take(&a, out.cap);
 
-  chdir("pages/");
-  DIR *dir = opendir(".");
-  for (struct dirent* f; (f = readdir(dir)); out.len = 0) 
-    ARENA_TEMP(a) {
-    if (f->d_type != DT_REG) continue;
-    str name = strc(f->d_name);
-    str md = read_file(&a, f->d_name);
-    Block *first = parse_md(&a, md);
+  rss.cap = MB(2);
+  rss.buf = Arena_take(&a, rss.cap);
+  append_str(&rss, rss_header);
 
-    // append_str(&out, header);
-    render_html(&out, first);
-    // append_str(&out, footer);
+  chdir("pages/writing");
+  {
+    DIR *dir = opendir(".");
+    for (struct dirent* f; (f = readdir(dir)); out.len = 0) 
+      ARENA_TEMP(a) {
+      if (f->d_type != DT_REG) continue;
+      
+      str name = strc(f->d_name);
+      str md = read_file(&a, f->d_name);
+      Block *first = parse_md(&a, md);
+      make_page(first);
 
-    printf("\n\n%.*s\n", (s32) name.len, (char*) name.str);
-    printf("%.*s\n", (s32) out.len, (char*) out.buf);
+      printf("\n\n%.*s\n", (s32) name.len, (char*) name.str);
+      printf("%.*s\n", (s32) out.len, (char*) out.buf);
+    }
+    closedir(dir);
   }
-  closedir(dir);
+
+  chdir("..");
+  {
+    DIR *dir = opendir(".");
+    for (struct dirent* f; (f = readdir(dir)); out.len = 0) 
+      ARENA_TEMP(a) {
+      if (f->d_type != DT_REG) continue;
+
+      str name = strc(f->d_name);
+      str md = read_file(&a, f->d_name);
+      Block *first = parse_md(&a, md);
+      make_page(first);
+
+      printf("\n\n%.*s\n", (s32) name.len, (char*) name.str);
+      printf("%.*s\n", (s32) out.len, (char*) out.buf);
+    }
+    closedir(dir);
+  }
 
   return 0;
 }
