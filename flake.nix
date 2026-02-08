6cdc56ae069a29a4232e3da1b1928480a43c5a0c
lf <contact@loganforman.com>
2026-02-07 21:40:13 -0700

    use lf flake

--- flake.nix
+++ flake.nix
@@ -2,59 +2,38 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    lf.url = "github:dev-dwarf/lf?ref=main";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, lf, ... }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [lf.overlay];
+    };
   in rec {
-
-    mkSTU = let 
-      mk = {name, src
-      , main ? name #default main src file is $name.cpp or $name.c
-      , buildInputs ? []
-      , flags ? ""
-      , debug ? false
-      , cpp ? false
-      , executable ? true
-      , installPhase ? ''
-        runHook preInstall;
-        install -Dt $out/bin ${name};
-        runHook postInstall;
-      ''
-      , ...}@args:
-        pkgs.stdenv.mkDerivation (args // {
-          inherit name src installPhase buildInputs;
-          doCheck = !debug;
-          dontStrip = debug;
-          allowSubstitutes = false;
-          buildPhase = 
-          ''${if cpp then "$CXX" else "$CC"} \
-            ${if executable then "-o ${name}" else "-o {name}.o -c"} \
-            ${src}/${main}.${if cpp then "cpp" else "c"} \
-            ${flags} ${if debug then "-g -O0" else "-O2"}
-          '';
-        });
-    in args: (mk args) // { debug = mk (args // { debug = true; }); };
-
     packages.${system} = pkgs // rec {
-      lf = pkgs.stdenv.mkDerivation rec {
-        name = "lf";
-        src = pkgs.fetchgit {
-          url = "https://github.com/dev-dwarf/lf.git";
-          hash = "sha256-Y+uriN4SmgdYF7w3jbuyLlZJj1JhHV4OID03KE4aHB4=";
-        };
-        dontBuild = true;
-        allowSubstitutes = false;
-        installPhase = '' mkdir -p $out/include && cp -r $src/* $out/include/'';
-      };
-
-      site = mkSTU rec {
-        buildInputs = [ lf ];
+      site = pkgs.mkSTU rec {
         name = "site";
         src = ./src;
       };
 
+      html = pkgs.stdenv.mkDerivation rec {
+        name = "html";
+        src = ./.; 
+        allowSubstitutes = false;
+        buildInputs = [ site ];
+        buildPhase = ''
+          mkdir -p $out/docs/writing
+          cp -r docs/ $out/
+          cp -r pages/ $out/
+          cd $out && site
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r docs/* $out
+        '';
+      };
     };
 
   };
